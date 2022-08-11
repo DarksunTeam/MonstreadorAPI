cf07e529c35843e10df553dd7258c8062138a017
Marcos Gonçalves <marcos.goncalves@avanade.com>
2022-08-11 14:04:02 -0300

    Spring Boot Architecture

---
+++ src/main/java/com/darksun/MonstreadorAPI/MonstreadorApiApplication.java
@@ -0,0 +1,13 @@
+package com.darksun.MonstreadorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonstreadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonstreadorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darksun/MonstreadorAPI/MonstreadorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darksun.MonstreadorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonstreadorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
