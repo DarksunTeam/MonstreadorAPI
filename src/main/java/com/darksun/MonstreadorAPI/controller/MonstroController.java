ea8f82834bf4d7c243a07eb0f9a2d7f5c406c271
Coppolaop <mag.junior@hotmail.com>
2022-08-12 17:53:01 -0300

    Listando monstros

---
+++ src/main/java/com/darksun/MonstreadorAPI/controller/MonstroController.java
@@ -0,0 +1,22 @@
+package com.darksun.MonstreadorAPI.controller;
+
+import com.darksun.MonstreadorAPI.entity.Monstro;
+import com.darksun.MonstreadorAPI.repository.MonstroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api")
+public class MonstroController {
+
+    @Autowired
+    MonstroRepository monstroRepository;
+
+    @GetMapping("/monstros")
+    public List<Monstro> listaMonstros(){
+        return monstroRepository.findAll();
+    }
+}
