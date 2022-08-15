43e19f835d29c2f4c6a9ce771e3233d111c82394
Coppolaop <mag.junior@hotmail.com>
2022-08-15 10:52:51 -0300

    Adicionando Swagger à aplicação

--- src/main/java/com/darksun/MonstreadorAPI/controller/MonstroController.java
+++ src/main/java/com/darksun/MonstreadorAPI/controller/MonstroController.java
@@ -2,6 +2,8 @@ package com.darksun.MonstreadorAPI.controller;
 
 import com.darksun.MonstreadorAPI.entity.Monstro;
 import com.darksun.MonstreadorAPI.repository.MonstroRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,30 +12,37 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST Monstros")
+@CrossOrigin(origins = "*")
 public class MonstroController {
 
     @Autowired
     MonstroRepository monstroRepository;
 
     @GetMapping("/monstros")
+    @ApiOperation( value = "Retorna todos os monstros cadastrados na base")
     public List<Monstro> listaMonstros() {
         return monstroRepository.findAll();
     }
     @GetMapping("/monstro/{id}")
+    @ApiOperation( value = "Busca um monstro na base pelo ID cadastrado")
     public Optional<Monstro> buscarMonstro(@PathVariable(value = "id") Long id){
         return monstroRepository.findById(id);
     }
     @PostMapping("/monstro")
+    @ApiOperation( value = "Cadastra um novo monstro na base")
     public Monstro salvaProduto(@RequestBody Monstro monstro) {
         return monstroRepository.save(monstro);
     }
 
     @DeleteMapping("/monstro")
+    @ApiOperation( value = "Apaga um monstro da base")
     public void deletaProduto(@RequestBody Monstro monstro) {
         monstroRepository.delete(monstro);
     }
 
     @PutMapping("/monstro")
+    @ApiOperation( value = "Altera informações de um monstro já cadastrado na base")
     public Monstro atualizaProduto(@RequestBody Monstro monstro) {
         return monstroRepository.save(monstro);
     }
