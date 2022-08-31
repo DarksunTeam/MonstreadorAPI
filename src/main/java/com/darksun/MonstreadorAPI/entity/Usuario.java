0da46588f99227ac9b4f4799682b8ddf79bdf968
Marcos Gonçalves <marcos.goncalves@avanade.com>
2022-08-31 19:42:40 -0300

    feat: Implementando Framework Lombok

--- src/main/java/com/darksun/MonstreadorAPI/entity/Usuario.java
+++ src/main/java/com/darksun/MonstreadorAPI/entity/Usuario.java
@@ -1,13 +1,21 @@
 package com.darksun.MonstreadorAPI.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serial;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Usuario implements Serializable {
 
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -17,28 +25,4 @@ public class Usuario implements Serializable {
 	private String login;
 	@JsonProperty( access = JsonProperty.Access.WRITE_ONLY )
 	private String password;
-
-	public Long getId( ) {
-		return id;
-	}
-
-	public void setId( Long id ) {
-		this.id = id;
-	}
-
-	public String getLogin( ) {
-		return login;
-	}
-
-	public void setLogin( String login ) {
-		this.login = login;
-	}
-
-	public String getPassword( ) {
-		return password;
-	}
-
-	public void setPassword( String password ) {
-		this.password = password;
-	}
 }
